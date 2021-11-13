d7c68ed2f6cd2380f4be5701ff9052bf6eade197
dazzul94 <dazzul94@gmail.com>
2021-11-13 17:11:30 +0900

    20211113_2

--- Part3/ex61_firestore/lib/main.dart
+++ Part3/ex61_firestore/lib/main.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
 
-void main() {
+late FirebaseApp fbApp;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  fbApp = await Firebase.initializeApp();
+  print('Initialized default app $fbApp');
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +30,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +37,122 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late CollectionReference members;
+  int seqNum = 0;
+  final ctlMyText1 = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    members = FirebaseFirestore.instance.collection('members');
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            ElevatedButton(
+              child: const Text('회원정보 추가', 
+                                  style: TextStyle(fontSize: 24),),
+              onPressed: () => doInsert(), 
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SizedBox(
+              width: 240,
+              child: TextField(
+                controller: ctlMyText1,
+              ),
+            ),
+            const SizedBox(height: 10,),
+            ElevatedButton(
+              child: const Text('회원정보 조회', 
+                                  style: TextStyle(fontSize: 24),),
+              onPressed: () => doSelectOne(), 
+            ),
+            ElevatedButton(
+              child: const Text('회원정보 수정', 
+                                  style: TextStyle(fontSize: 24),),
+              onPressed: () => doUpdate(), 
+            ),
+            ElevatedButton(
+              child: const Text('회원정보 삭제', 
+                                  style: TextStyle(fontSize: 24),),
+              onPressed: () => doDelete(), 
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<void> doInsert() async {
+    // 회원정보를 firestore 문서로 추가
+    String sNum = ctlMyText1.text;
+    int nNum = int.parse(sNum);
+    String sId = 'member$nNum';
+    print("sId: $sId");
+
+    var documentReference = await members.doc(sId);
+    print(documentReference == null);
+
+    if(documentReference == null) {
+      members.doc(sId).set({
+        'pw': '1234',
+        'email': 'test1@test.com',
+      });
+    } else {
+      print('회원 정보가 이미 존재합니다.');
+    }
+  }
+
+  Future<void> doSelectOne() async {
+    String sNum = ctlMyText1.text;
+    int nNum = int.parse(sNum);
+    String sId = 'member$nNum';
+
+    var documentSnapshot = await members.doc(sId).get();
+    print(documentSnapshot != null);
+
+    if(documentSnapshot != null) {
+      String pw = documentSnapshot.get('pw');
+      String email = documentSnapshot.get('email');
+      print('pw : $pw');
+      print('email: $email');
+    } else {
+      print('회원 정보가 존재하지 않습니다.');
+    }
+  }
+
+  Future<void> doUpdate() async {
+    String sNum = ctlMyText1.text;
+    int nNum = int.parse(sNum);
+    String sId = 'member$nNum';
+
+    var documentReference = await members.doc(sId);
+    if(documentReference != null) {
+      members.doc(sId).update({
+        'email': 'xxxx@test.com',
+      });
+    } else {
+      print('회원 정보가 존재하지 않습니다.');
+    }
+  }
+
+  Future<void> doDelete() async {
+    String sNum = ctlMyText1.text;
+    int nNum = int.parse(sNum);
+    String sId = 'member$nNum';
+
+    var documentReference = await members.doc(sId);
+    if(documentReference != null) {
+      documentReference.delete();
+    } else {
+      print("회원 정보가 존재하지 않습니다.");
+    }
   }
 }
